c1ee4726722ac94c13f7e797af72ce0a4ed90912
Naman Jha <itsnaman0007@gmail.com>
2025-07-23 14:16:10 +0000

    Improved navbar UI: Dark mode, Hover effects, active link highlight

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      "88b39973-a5f8-43c2-b1af-e0eacbc85c99-00-2welbeymh8max.pike.replit.dev"
+    ]
+  },
+  plugins: [react(), tailwindcss()]
+});
